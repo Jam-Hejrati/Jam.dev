c3a00e2d61fcf908c1dae0637b6bf028e2fa6490
jam <jamhejratipro@gmail.com>
2022-09-08 14:46:24 +0430

    navbar start

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from './components/Header';
+import MenuBar from './components/MenuBar';
 import './App.css';
 
 function App() {
   return (
   <div className='bg-gray-800'>
+    <MenuBar />
     <Header/>
   </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import ProfileImage from "../assets/images/profile.jpg";
+// import MenubBar from "./MenuBar";
 
 const Header = function () {
   return (
@@ -9,9 +10,15 @@ const Header = function () {
         className="w-72 h-72 rounded-full lg:h-full lg:w-max lg:rounded-none my-12 lg:m-0 border-8 lg:border-none border-slate-700"
       />
       <div className="flex flex-col items-center lg:mx-auto lg:items-start lg:px-20">
+        {/* <MenubBar /> */}
         <h1 className="text-5xl text-amber-600">Jam Hejrati</h1>
-        <h2 className="text-3xl text-slate-100 my-5 lg:ml-4 lg:mt-5">Frontend Web Developer</h2>
-        <p className="text-lg text-slate-400 mt-4 text-center px-16 lg:p-0 lg:text-left">I'm a passionate and hardwork front-end web developer who is trying to make things easier for his self and who's around him.</p>
+        <h2 className="text-3xl text-slate-100 my-5 lg:ml-4 lg:mt-5">
+          Frontend Web Developer
+        </h2>
+        <p className="text-lg text-slate-400 mt-4 text-center px-16 lg:p-0 lg:text-left">
+          I'm a passionate and hardwork front-end web developer who is trying to
+          make things easier for his self and who's around him.
+        </p>
       </div>
     </div>
   );
