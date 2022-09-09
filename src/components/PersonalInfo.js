160a9e07280fb6f72c1e9a6fa5a2647f859e0fc6
Jam Hejrati <jamhejratipro@gmail.com>
2022-09-10 01:18:59 +0430

    info section responsive fixed

--- src/components/PersonalInfo.js
+++ src/components/PersonalInfo.js
@@ -3,15 +3,21 @@ import InfoSection from "./InfoSection";
 const PersonalInfo = function (props) {
   return (
     <div id="about" className="my-10 lg:mt-40 flex items-center flex-col">
-      <span className="mx-auto mb-6 pb-2 border-b border-gray-400 text-3xl text-slate-200 text-center">
+      <span className="mx-auto mb-8 lg:mb-16 pb-2 px-4 border-b border-gray-400 text-3xl text-slate-200 text-center">
         Personal Info
       </span>
-      <InfoSection title="First Name" info={props.info["firstName"]} />
-      <InfoSection title="Last Name" info={props.info["lastName"]} />
-      <InfoSection title="Age" info={props.info["age"]} />
-      <InfoSection title="Address" info={props.info["address"]} />
-      <InfoSection title="Language" info={props.info["language"]} />
-      <InfoSection title="Open To Work" info={props.info["openToWork"]} />
+      <div className="flex flex-col items-center lg:flex-row lg:gap-44">
+        <div className="flex flex-col items-center">
+          <InfoSection title="First Name" info={props.info["firstName"]} />
+          <InfoSection title="Last Name" info={props.info["lastName"]} />
+          <InfoSection title="Age" info={props.info["age"]} />
+        </div>
+        <div className="flex flex-col items-center">
+          <InfoSection title="Address" info={props.info["address"]} />
+          <InfoSection title="Language" info={props.info["language"]} />
+          <InfoSection title="Open To Work" info={props.info["openToWork"]} />
+        </div>
+      </div>
     </div>
   );
 };
